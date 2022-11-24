a231ee7942b1ed4baeafdc0c47f3046d85a17275
corranmac <corranmac@hotmail.co.uk>
2022-11-24 04:48:27 +0000

    simplify download.py for t2i
    Removed other model types to speed up deployment

--- download.py
+++ download.py
@@ -8,17 +8,11 @@ def download_model():
     # do a dry run of loading the huggingface model, which will download weights at build time
     #Set auth token which is required to download stable diffusion model weights
     HF_AUTH_TOKEN = os.getenv("HF_AUTH_TOKEN")
-    os.system("git clone https://github.com/Stability-AI/stablediffusion/")
+    os.system("git clone https://github.com/Stability-AI/stablediffusion")
     sys.path.insert(1,"stablediffusion")
-    os.chdir("stablediffusion")
-    os.mkdir("checkpoints")
-    os.mkdir("midas")
-    os.chdir("checkpoints")
-    hf_hub_download(repo_id="stabilityai/stable-diffusion-2-base", filename="512-base-ema.ckpt",token=str(HF_AUTH_TOKEN))
+    os.mkdir("stablediffusion/checkpoints")
+    os.chdir("stablediffusion/checkpoints")
     hf_hub_download(repo_id="stabilityai/stable-diffusion-2", filename="768-v-ema.ckpt",token=str(HF_AUTH_TOKEN))
-    os.chdir("..")
-    os.chdir("midas")
-    hf_hub_download(repo_id="stabilityai/stable-diffusion-2-depth", filename="512-depth-ema.ckpt",token=str(HF_AUTH_TOKEN))
     os.chdir("..")
 if __name__ == "__main__":
     download_model()
