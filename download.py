a443856e99f1349ae25814d4ff8b40f7bda77bba
corranmac <corranmac@hotmail.co.uk>
2022-11-24 04:28:31 +0000

    fix

--- download.py
+++ download.py
@@ -16,9 +16,9 @@ def download_model():
     os.chdir("checkpoints")
     hf_hub_download(repo_id="stabilityai/stable-diffusion-2-base", filename="512-base-ema.ckpt",token=str(HF_AUTH_TOKEN))
     hf_hub_download(repo_id="stabilityai/stable-diffusion-2", filename="768-v-ema.ckpt",token=str(HF_AUTH_TOKEN))
-    os.chdir(".")
+    os.chdir("~")
     os.chdir("midas")
     hf_hub_download(repo_id="stabilityai/stable-diffusion-2-depth", filename="512-depth-ema.ckpt",token=str(HF_AUTH_TOKEN))
-    os.chdir(".")
+    os.chdir("~")
 if __name__ == "__main__":
     download_model()
